6e122fce96dee47a4b5cf53d2c2b75bf599cdc20
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-06-02 01:25:00 +0300

    v1.04

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -2,24 +2,27 @@ import React, { PureComponent } from 'react';
 import styles from './Balance.styl';
 import hIcon from 'media/h.svg';
 import Content from 'components/Content/';
+import container from "components/container";
 
-export default class Balance extends PureComponent {
+class Balance extends PureComponent {
   render() {
+    const { trading_capital_currency, trading_capital, balance, on_hold } = this.props.data;
+    
     return (
       <Content>
         <div className={styles.balance}>
           <div className={styles.capital}>Trading Capital</div>
           <div className={styles.info}>
-            <div className={styles.tradeBalance}>1.00865 BTC</div>
+            <div className={styles.tradeBalance}>{trading_capital} {trading_capital_currency}</div>
             <div className={styles.details}>
               <div className={styles.item}>
                 <div className={styles.text}>balance :</div>&nbsp;
-                <div className={styles.amount}>10850</div>&nbsp;
+                <div className={styles.amount}>{balance}</div>&nbsp;
                 <img src={hIcon} className={styles.hIcon} alt="icon"/>
               </div>
               <div className={styles.item}>
                 <div className={styles.text}>on hold :</div>&nbsp;
-                <div className={styles.amount}>2400</div>&nbsp;
+                <div className={styles.amount}>{on_hold}</div>&nbsp;
                 <img src={hIcon} className={styles.hIcon} alt="icon"/>
               </div>
             </div>
@@ -30,3 +33,5 @@ export default class Balance extends PureComponent {
     );
   }
 }
+
+export default container(Balance);
