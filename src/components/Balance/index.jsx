7f7ddb75ac193e8244c92914c2dc54fa08720332
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-06-01 16:34:51 +0300

    end topBlock

--- src/components/Balance/index.jsx
+++ src/components/Balance/index.jsx
@@ -1,18 +1,29 @@
 import React, { PureComponent } from 'react';
-import cn from 'classnames';
 import styles from './Balance.styl';
-import { Icon } from 'antd';
+import hIcon from 'media/h.svg';
 import Content from 'components/Content';
-
-const mainColor = '#546076';
 
 export default class Balance extends PureComponent {
   render() {
     return (
       <Content>
         <div className={styles.balance}>
-          <div className={styles.top}>Trading Capital</div>
-          <div className={styles.bottom}>1.00865 BTC</div>
+          <div className={styles.capital}>Trading Capital</div>
+          <div className={styles.info}>
+            <div className={styles.tradeBalance}>1.00865 BTC</div>
+            <div className={styles.details}>
+              <div className={styles.item}>
+                <div className={styles.text}>balance :</div>&nbsp;
+                <div className={styles.amount}>10850</div>&nbsp;
+                <img src={hIcon} className={styles.hIcon} alt="icon"/>
+              </div>
+              <div className={styles.item}>
+                <div className={styles.text}>on hold :</div>&nbsp;
+                <div className={styles.amount}>2400</div>&nbsp;
+                <img src={hIcon} className={styles.hIcon} alt="icon"/>
+              </div>
+            </div>
+          </div>
         </div>
 
       </Content>
